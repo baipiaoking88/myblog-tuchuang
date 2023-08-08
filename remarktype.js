b14581d6a5694f20afa2157255ae0fc4f4e801ff
baipiaoking88 <108050351+baipiaoking88@users.noreply.github.com>
2023-08-08 22:21:33 +0800

    Create remarktype.js

---
+++ remarktype.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function() {
+      var bottomDiv = document.getElementById('bottomDiv');
+      var body = document.getElementsByTagName('body')[0];
+      body.appendChild(bottomDiv);
+});
